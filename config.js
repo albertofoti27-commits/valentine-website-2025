63dbbf76d26e73c3bd879837f7e6585b3651fc7f
albertofoti27-commits <alberto.foti27@gmail.com>
2026-02-11 12:14:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -76,8 +76,8 @@ const CONFIG = {
     // Background Music (Optional)
     // Add your own music URL after getting proper licenses
     music: {
-        enabled: true,                     // Music feature is enabled
-        autoplay: true,                    // Try to autoplay (note: some browsers may block this)
+        enabled: false,                     // Music feature is enabled
+        autoplay: false,                    // Try to autoplay (note: some browsers may block this)
         musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
