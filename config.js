e9f4b483442419591297b416eed2c89d7e42e689
albertofoti27-commits <alberto.foti27@gmail.com>
2026-02-11 11:50:31 +0100

    Update config.js

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ const CONFIG = {
         second: {
             text: "Quanto ci vuoi andare??",                          // For the love meter
             startText: "Tanto!",                                   // Text before the percentage
-            nextBtn: "Tantissimooooo❤️"                                         // Text for the next button
+            nextBtn: "Tantissimooooo"                                         // Text for the next button
         },
         third: {
             text: "Quindi vuoi essere la mia valentina giorno 13 Febbraio 2026???🌹", // The big question!
@@ -42,16 +42,16 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "Fammi capire un attimo quanto ci vuoi andare..... " // Shows when they go past 5000%
+        high: "ci toccherà ordinare veramente tanto sushi allora",              // Shows when they go past 1000%
+        normal: "Così poco?"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
-        emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
+        title: "Perfetto!!! Allora ci vediamo giorno 13 Febbraio ore 21:15 da IZU!!!",
+        message: "Non tardare, fatti super bona mi raccomando!!!",
+        emojis: "💖🤗"  // These will bounce around
     },
 
     // Color scheme for the website
@@ -76,7 +76,7 @@ const CONFIG = {
     // Background Music (Optional)
     // Add your own music URL after getting proper licenses
     music: {
-        enabled: true,                     // Music feature is enabled
+        enabled: false,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
         musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
